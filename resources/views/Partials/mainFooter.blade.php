34302683f06fdb401da495327f02ee603ff3796e
roozbeh <45459584+roozbehshafieebafti@users.noreply.github.com>
2019-08-22 22:52:53 +0430

    working on menu content

--- resources/views/Partials/mainFooter.blade.php
+++ resources/views/Partials/mainFooter.blade.php
@@ -1,6 +1,15 @@
-<div class="Footer">
+<div class="Footer" style="overflow-x: hidden;overflow-y: hidden;">
     <div style="background-color: #790000; height:20px;"></div>
-    <div class="main-footer">
+    <div class="main-footer" style="overflow-x: hidden;overflow-y: hidden;">
+        <div class="footer-right-container">
+            <div class="right-yellow-div"></div>
+        </div>
+        <div class="right-yellow-border">
+                sdfsdf
+        </div>
+        <div class="footer-middle-container"></div>
+
+        <div class="footer-left-container"></div>
 
     </div>
 </div>
