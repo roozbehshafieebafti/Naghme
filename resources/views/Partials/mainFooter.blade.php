488ee2c2a51c3f0922016bb36d2b0987ce8b1df6
roozbeh <45459584+roozbehshafieebafti@users.noreply.github.com>
2019-08-25 00:07:53 +0430

    footer design

--- resources/views/Partials/mainFooter.blade.php
+++ resources/views/Partials/mainFooter.blade.php
@@ -1,13 +1,29 @@
 <div class="Footer" style="overflow-x: hidden;overflow-y: hidden;">
     <div style="background-color: #790000; height:20px;"></div>
-    <div class="main-footer" style="overflow-x: hidden;overflow-y: hidden;">
-        <div class="footer-right-container">
+    <div class="main-footer" >
+        <div class="footer-right-container" style="overflow-x: hidden;overflow-y: hidden;">
             <div class="right-yellow-div"></div>
+            <div class="right-yellow-border"></div>
+            <div class="right-gray-border"></div>
+            <p class="right-paragraph">اطلاعات تماس</p>
         </div>
-        <div class="right-yellow-border">
-                sdfsdf
+        
+        <div class="footer-middle-container">
+            <ul class="footer-middle-ul-list">
+                <li>
+                    <label class="text-white">سامانه پیامکی انجمن : 20003413871001</label>
+                    <img class="bg-danger" src="{{ config("app.url")."picture/assets/sms.svg" }}" alt="sms" width="70px">
+                </li>                
+                <li>
+                    <label class="text-white">سامانه پیامکی انجمن : 20003413871001</label>
+                    <img class="bg-danger" src="{{ config("app.url")."picture/assets/info.svg" }}" alt="info" width="48px">
+                </li>
+                <li>
+                    <label class="text-white">سامانه پیامکی انجمن : 20003413871001</label>
+                    <img class="bg-danger" src="{{ config("app.url")."picture/assets/email.svg" }}" alt="email" width="50px">
+                </li>
+            </ul>
         </div>
-        <div class="footer-middle-container"></div>
 
         <div class="footer-left-container"></div>
 
