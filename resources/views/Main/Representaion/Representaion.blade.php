2fc5a257798f877dbdc872cd8f2c0456b501f48a
roozbeh <45459584+roozbehshafieebafti@users.noreply.github.com>
2019-11-07 19:01:39 +0330

    representains page is done

--- resources/views/Main/Representaion/Representaion.blade.php
+++ resources/views/Main/Representaion/Representaion.blade.php
@@ -6,13 +6,17 @@
         <div class="container">
             <div class="row">
                 @foreach ($Represent as $item)
-                    <div class="col-xl-6 col-lg-6 col-12 ">
+                    <div class="col-xl-6 col-lg-8 col-md-8 col-12 main-representaion-content">
                         <div class="Representaion-border row">
                             <div class="Representaion-content-container">
-                                <img class="representaion-element " src="{{ config('app.url').'picture/assets/representation.svg'}}" alt="">
-                                <p class="Representaion-title">{{ $item->representation_title }}</p>
+                                <a class="representation-link" href="{{ route("Representaion_Read_More",$item->representation_title) }}">
+                                    <div>
+                                        <img class="representaion-element " src="{{ config('app.url').'picture/assets/representation.svg'}}" alt="">
+                                        <p class="Representaion-title">{{ $item->representation_title }}</p>
+                                    </div>
+                                </a>
                             </div>
-                            <div class="col-7" style="position:relative;bottom: 10px;">
+                            <div class="col-xl-6 col-lg-7 col-md-6 col-6 Representaion-content-container-2" style="position:relative;bottom: 10px;">
                                 <div class="Representaion-info-container text-white pl-3 pr-3">
                                     <p>
                                         <span style="color:#f6a619">سرپرست نمایندگی:</span>
@@ -31,7 +35,7 @@
                                 </div>
                                 <div class="Representaion-read-more">
                                     <a class="text-dark" href="{{ route("Representaion_Read_More",$item->representation_title) }}">ادامه مطلب</a>
-                                    <i class="fas fa-long-arrow-alt-left pt-1"></i>
+                                    <i class="fas fa-long-arrow-alt-left pt-1 mr-5"></i>
                                 </div>
                             </div>
                         </div>
